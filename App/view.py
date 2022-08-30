a3738d0a495f2515fbf3786a5925fb8bfb255b5b
juan-jerez <j.jerezz@uniandes.edu.co>
2022-08-30 18:00:38 -0500

    Cambios menu

--- App/view.py
+++ App/view.py
@@ -37,8 +37,14 @@ operación solicitada
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
-
+    print("2- Listar las películas estrenadas en un periodo")
+    print("3- Listar programas de televisión agregados en un periodo de tiempo")
+    print("4- Encontrar contenido donde participa un actor")
+    print("5- Encontrar contenido por un género especifico")
+    print("6- Encontrar contenido producido en un país")
+    print("7- Encontrar contenido con un director involucrado")
+    print("8- Listar el TOP (N) de los géneros con más contenido")
+    print("9- Listar el TOP (N) de los actores con más participaciones en contenido")
 catalog = None
 
 """
@@ -51,7 +57,54 @@ while True:
         print("Cargando información de los archivos ....")
 
     elif int(inputs[0]) == 2:
-        pass
+        lim_inf= int(input("introduzca el limite inferior para el que quiere buscar las peliculas: "))
+        lim_sup= int(input("introduzca el limite superior para el que quiere buscar las peliculas: "))
+        print("Buscando ....")
+    #    peliculas= controller.listar_peliculas_estrenadas_en un periodo(lim_inf,lim_sup)
+        print(peliculas)
+
+    elif int(inputs[0]) == 3:
+        fecha_inicial= input("introduzca la fecha inicial, con formato: %B %d, %Y : ")
+        fecha_final= input("introduzca la fecha final, con formato: %B %d, %Y : ")
+        print("Buscando....")
+    #    programas= controller.listar_programas_agregados en un periodo(fecha_inicial,fecha_final)
+        print(programas)
+
+    elif int(inputs[0]) == 4:
+        nombre_actor= input("introduzca el nombre del autor que desea consultar: ")
+        print("Buscando ....")
+    #    contenido_actor= controller.encontrar_contenido_x_actor(nombre_actor)
+        print(contenido_actor)
+
+    elif int(inputs[0]) == 5:
+        genero= input("introduzca el genero que desea buscar: ")
+        print("Buscando ....")
+    #    contenido_genero= controller.encontrar_contenido_x_genero(genero)
+        print(contenido_genero)
+
+    elif int(inputs[0]) == 6:
+        pais= input("introduzca el pais a consultar: ")
+        print("Buscando ....")
+    #    contenido_pais= controller.contenido_x_pais(pais)
+        print(contenido_pais) 
+
+    elif int(inputs[0]) == 7:
+        director= input("introduzca el director a consultar")
+        print("Buscando ....")
+    #    contenido_x_director= controller.contenido_x_director(director)
+        print(contenido_x_director)
+
+    elif int(inputs[0]) == 8:
+        n= int(input("ingrese el numero n de top generos que quiere buscar"))
+        print("Buscando ....")
+    #    top_n_generos= controller.top_n_generos(n)
+        print(top_n_generos)
+
+    elif int(inputs[0]) == 9:
+        n= int(input("introduzca el n top de actores que desea consultar"))
+        print("Buscando ....")
+    #    top_n_actores= controller.top_n_actores(n)
+        print(top_n_actores)
 
     else:
         sys.exit(0)
